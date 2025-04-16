c750751c3258d0634b12f523d2b753372be3ba94
jyneeweenie <jynemarie1@gmail.com>
2025-04-16 10:54:26 +0300

    done

--- ASSIGNMENT/main.py
+++ ASSIGNMENT/main.py
@@ -1,1 +1,7 @@
-number1 
+def substrings(s):
+    return [s[i:j] for i in range(len(s)) for j in range(i+1, len(s)+1)]
+
+arr = input("Enter strings: ").split()
+
+for s in arr:
+    print(f"Substrings of '{s}':", substrings(s))
